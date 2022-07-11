730e7f7493e9739a1c9814aca9eaf9204b017774
hp196@naver.com <hp196@naver.com>
2022-07-11 19:08:08 +0900

    2022-07-11

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+    @Test
+    public void 상품주문() throws Exception {
+
+    }
+
+}
